2d41133ebdab8f384a830b2d31092e0b61a633a9
cecep <cecepjanuardi@outlook.com>
2025-04-12 10:33:42 +0700

    fix

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -91,7 +91,7 @@ func (r *Routes) setupPostRoutes(v1 *echo.Group) {
 
 func (r *Routes) setupAuthRoutes(v1 *echo.Group) {
 	auth := v1.Group("/auth")
-	confratelimit := echomidleware.RateLimiterMemoryStoreConfig{Rate: rate.Limit(5), ExpiresIn: 5 * time.Minute}
+	confratelimit := echomidleware.RateLimiterMemoryStoreConfig{Rate: rate.Limit(5), ExpiresIn: 5 * time.Minute, Burst: 5}
 	{
 		auth.POST("/register", r.authHandler.Register)
 		auth.POST("/login", r.authHandler.Login, echomidleware.RateLimiter(echomidleware.NewRateLimiterMemoryStoreWithConfig(confratelimit)))
