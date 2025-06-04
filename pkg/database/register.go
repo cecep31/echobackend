a13dda6d6bcee8853b6668ed2b0fe9b5797a3259
cecep <cecepjanuardi@outlook.com>
2025-06-04 15:27:07 +0700

    refactor: remove obsolete register.go file after migrating to GORM

--- pkg/database/register.go
+++
@@ -1,4 +0,0 @@
-package database
-
-// This file was used for Bun ORM model registration and is no longer needed after migrating to GORM.
-// GORM's AutoMigrate feature (typically called in database setup) handles model registration and schema migration.
